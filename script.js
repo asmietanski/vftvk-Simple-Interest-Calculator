ee127d3bdf77677c3d46843031b7d74856494417
Adam Smietanski <adam.smietanski@pl.ibm.com>
2021-07-20 13:56:17 +0200

    lab completed

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 function compute()
 {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    if (principal <= 0) {
+        alert("Enter a prositive number");
+        document.getElementById("principal").focus();
+        return false;
+
+    }
+    
+
     p = document.getElementById("principal").value;
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+"\<span class='highlight'\>"+interest+"\</span\>"+",\<br\>in the year "+year+"\<br\>";
     
 }
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  } 
         
